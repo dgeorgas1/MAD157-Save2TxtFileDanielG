ad0b6a24ce35fdc2bddc6ae25b942191f2f14e76
Daniel Georgas <dgeorgas@students.mchenry.edu>
2024-03-10 19:26:40 -0500

    Commit changes

--- Save2TxtFile/ViewController.swift
+++ Save2TxtFile/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var typeTF: UITextView!
+    @IBOutlet weak var displayTF: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        typeTF.text = "Type the new text here"
+        displayTF.text = ""
     }
-
-
+    
+    @IBAction func readBtn(_ sender: UIButton) {
+        let fm = FileManager.default
+        let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = urls.last?.appendingPathComponent("file.txt")
+        
+        do {
+            let fileContent = try String(contentsOf: url!, encoding: String.Encoding.utf8)
+            
+            displayTF.text = fileContent
+        }
+        catch {
+            print("Error reading file")
+        }
+    }
+    
+    @IBAction func writeBtn(_ sender: UIButton) {
+        let fm = FileManager.default
+        let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = urls.last?.appendingPathComponent("file.txt")
+        
+        do {
+            try typeTF.text.write(to: url!, atomically: true, encoding: String.Encoding.utf8)
+        }
+        catch {
+            print("Error writing to file")
+        }
+    }
 }
-
